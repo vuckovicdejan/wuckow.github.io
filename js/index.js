0c1bdf36944b569401eb8732e8bd518ee93e9d31
Dejan Vuckovic <vuckovic.dejan1987@gmail.com>
2018-07-20 15:17:25 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,9 +2,8 @@ $( document ).ready(function() {
   
   
   
-    $(document).on('scroll', function () {
-        $('.nav-bar').css('background-color', 'Aquamarine');
-    });
+      $('.navbar a').smoothScroll();
+
 
   
   
